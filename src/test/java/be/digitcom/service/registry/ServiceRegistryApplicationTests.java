044ae8d6c8001d9495829f9059fb7da989716004
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-15 16:06:12 +0200

    Env in pom.xml. Disable test

--- src/test/java/be/digitcom/service/registry/ServiceRegistryApplicationTests.java
+++ src/test/java/be/digitcom/service/registry/ServiceRegistryApplicationTests.java
@@ -1,5 +1,6 @@
 package be.digitcom.service.registry;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ServiceRegistryApplicationTests {
 
 	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
